8fa309d41d1374b48ee958ede8c8b169f67529af
Mark Lysykh <lismark88@gmail.com>
2020-06-01 17:44:00 +0300

    init

---
+++ Assets/Scripts/GameClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClass : MonoBehaviour
+{
+    void Start()
+    {
+        var gc = new GameCore(4,6);
+        gc.Move(0, 0, GameCore.Direction.Left);
+
+        Debug.Log(gc);
+    }
+
+}
---
+++ Assets/Scripts/GameCore.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+public class GameCore
+{
+    private int[,] gameField;
+    private int sizeX;
+    private int sizeY;
+
+    public GameCore(int sizeX, int sizeY)
+    {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        gameField = new int[sizeX, sizeY];
+    }
+
+    public enum Direction
+    {
+        Nope,
+        Left,
+        Up,
+        Right,
+        Down
+    }
+
+    public bool Move(int i, int j, Direction dir)
+    {
+        switch(dir)
+        {
+            case Direction.Nope:
+                Debug.Log("Stop");
+                return false;
+
+            case Direction.Left:
+                Debug.Log("Left");
+                return true;
+
+            case Direction.Right:
+                Debug.Log("Right");
+                return true;
+
+            case Direction.Up:
+                Debug.Log("Up");
+                return true;
+
+            case Direction.Down:
+                Debug.Log("Down");
+                return true;
+
+            default: return false;
+        }
+    }
+
+    private bool IsComplete()
+    {
+        for(int i = 0; i < sizeX; i++)
+        {
+            for(int j = 0; j < sizeY; j++)
+            {
+                if(gameField[i,j] != gameField[i+1, j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
