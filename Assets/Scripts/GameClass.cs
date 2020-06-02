22f062d94b899044cfb0d1b4b47585df924c44e2
Mark Lysykh <lismark88@gmail.com>
2020-06-02 19:24:23 +0300

    horizontal scroll added

--- Assets/Scripts/GameClass.cs
+++ Assets/Scripts/GameClass.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +7,20 @@ public class GameClass : MonoBehaviour
     void Start()
     {
         var gc = new GameCore(4,6);
-        gc.Move(0, 0, GameCore.Direction.Left);
+        gc.Move(1, 0, GameCore.Direction.Left);
+        Debugging(gc.gameField);
 
-        Debug.Log(gc);
+    }
+
+    private void Debugging(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Debug.Log(array[i, j]);
+            }
+        }
     }
 
 }
--- Assets/Scripts/GameCore.cs
+++ Assets/Scripts/GameCore.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using System;
 public class GameCore
 {
-    private int[,] gameField;
+    public int[,] gameField;
     private int sizeX;
     private int sizeY;
 
@@ -10,8 +11,20 @@ public class GameCore
         this.sizeX = sizeX;
         this.sizeY = sizeY;
         gameField = new int[sizeX, sizeY];
+        InitField();
     }
 
+    private void InitField()
+    {
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                gameField[i, j] = i;
+                
+            }
+        }
+    }
     public enum Direction
     {
         Nope,
@@ -21,31 +34,55 @@ public class GameCore
         Down
     }
 
-    public bool Move(int i, int j, Direction dir)
+    public bool Move(int x, int y, Direction dir)
     {
         switch(dir)
         {
             case Direction.Nope:
-                Debug.Log("Stop");
+                Console.WriteLine("Stop");
                 return false;
 
             case Direction.Left:
-                Debug.Log("Left");
+                ShiftHorizontal(x, y, dir);
                 return true;
 
             case Direction.Right:
-                Debug.Log("Right");
+                ShiftHorizontal(x, y, dir);
                 return true;
 
             case Direction.Up:
-                Debug.Log("Up");
+                Console.WriteLine("Up");
                 return true;
 
             case Direction.Down:
-                Debug.Log("Down");
+                Console.WriteLine("Down");
                 return true;
 
             default: return false;
+        }
+    }
+
+    private void ShiftHorizontal(int x, int y, Direction dir)
+    {
+
+        if(dir == Direction.Left)
+        {
+          var tmp = gameField[x,sizeX-1];
+          for(int i = sizeX -1 ; i > 0; i--)
+            {
+                gameField[i, y] = gameField[i - 1, y];
+            }
+            gameField[0, y] = tmp;
+        }
+        
+        if(dir == Direction.Right)
+        {
+            var tmp = gameField[0,y];
+            for(int i = 0; i < sizeX -1; i++)
+            {
+                gameField[i, y +1] = gameField[i, y];
+            }
+            gameField[0, y] = tmp;
         }
     }
 
